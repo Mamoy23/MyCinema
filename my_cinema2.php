f2440bf0a77f5d23a3da55fcbea0b796515b0f45
Marine <marine.moynet@epitech.eu>
2018-12-19 15:54:09 +0100

    Recherche client, abonnement, historique terminées + ajout entrée historique

--- my_cinema2.php
+++ my_cinema2.php
@@ -18,8 +18,12 @@
     $stmt->execute();
     $distribs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    $sql = 'SELECT * FROM film ORDER BY titre ASC';
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute();
+    $films = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     include('test.php');
-    
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,12 +37,12 @@
 <body>
 
     <div class="container">
-        <h1 class="text-dark text-center">Rechercher un film</h1>
-        <nav class="navbar-dark bg-dark mt-3 mb-3 p-3">
+        <h1 class="text-dark m-2">Rechercher un film</h1>
+
             <form method="POST" action="my_cinema2.php" class="form-inline">
                 <input type="text" name="nom" placeholder="Titre de film" class="form-control m-2">  
 
-                <select name="genre" id="genre" class="custom-select" m-2>
+                <select name="genre" id="genre" class="custom-select m-2">
                     <option value="">Sélectionnez un genre</option>
                     <?php foreach($genres as $genre): ?>
                     <option value="<?= $genre['id_genre']; ?>"> <?= $genre['nom']; ?></option>
@@ -52,33 +56,160 @@
                 <?php endforeach; ?>
                 </select>
 
-                <input type="submit" value="Rechercher" class="btn btn-light m-2"> 
+                <input type="submit" value="Rechercher" class="btn btn-dark m-2"> 
             </form>
-        </nav>
-        <table class="table table-dark">
-        <thead>
-            <tr>
-            <th scope="col">Titre</th>
-            <th scope="col">Genre</th>
-            <th scope="col">Synopsis</th>
-            <th scope="col">Durée</th>
-            <th scope="col">Jusqu'au</th>
-            <th scope="col">Distributeur</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($films as $film): ?>
-            <tr>
-            <td><?= $film["titre"]?></td>
-            <td><?= $film["nom_genre"]?></td>
-            <td><?= $film["resum"]?></td>
-            <td><?= $film["duree_min"]?> mins</td>
-            <td><?= $film["date_fin_affiche"]?></td>
-            <td><?= $film["nom_distrib"]?></td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </div>
 
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                <th scope="col">Titre</th>
+                <th scope="col">Genre</th>
+                <th scope="col">Synopsis</th>
+                <th scope="col">Durée</th>
+                <th scope="col">Jusqu'au</th>
+                <th scope="col">Distributeur</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if(!empty($_POST['nom']) || !empty($_POST['genre']) || !empty($_POST['distribution'])): ?>
+                <?php foreach($movies as $film): ?>
+                <tr>
+                <td><?= $film["titre"]?></td>
+                <td><?= $film["nom_genre"]?></td>
+                <td><?= $film["resum"]?></td>
+                <td><?= $film["duree_min"]?> mins</td>
+                <td><?= $film["date_fin_affiche"]?></td>
+                <td><?= $film["nom_distrib"]?></td>
+                </tr>
+                <?php endforeach; ?>
+                <?php endif; ?>
+            </tbody>
+        </table>
+
+        <h1 class="text-primary m-2">Rechercher un client</h1>
+
+        <form method="POST" action="my_cinema2.php" class="form-inline">
+            <input type="text" name="client" placeholder="Nom et/ou prénom" class="form-control m-2">
+            <input type="submit" value="Rechercher" class="btn btn-primary m-2">
+        </form>
+
+        <table class="table table-primary">
+            <thead>
+            <tr>
+                <th scope="col">Nom</th>
+                <th scope="col">Prénom</th>
+                <th scope="col">Email</th>          
+                <th scope="col">Dernière visite</th>
+                <th scope="col">Ville</th> 
+                <th scope="col">N° Client</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php if(!empty($_POST['client'])) : ?>
+                <?php foreach($customers as $client): ?>
+                <tr>
+                    <td><?= $client['nom'] ?></td>
+                    <td><?= $client['prenom'] ?></td>
+                    <td><?= $client['email'] ?></td>
+                    <td><?= $client['date_dernier_film'] ?></td>
+                    <td><?= $client['ville'] ?></td>
+                    <td><?= $client['id_membre'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+                <?php endif; ?>
+            </tbody>
+        </table>
+
+        <h1 class="text-danger m-2">Abonnements</h1>
+
+        <form method="POST" action="my_cinema2.php" class="form-inline">
+            <input type="text" name="abos" placeholder="Nom et/ou prénom" class="form-control m-2">
+            <input type="submit" value="Rechercher" class="btn btn-danger m-2">
+        </form>
+
+        <table class="table table-danger">
+            <thead>
+            <tr>
+                <th scope="col">Nom</th>
+                <th scope="col">Prénom</th>
+                <th scope="col">N°client</th>
+                <th scope="col">Abonnement</th>
+                <th scope="col">Durée</th>
+                <th scope="col">Inscription</th>          
+                <th scope="col">Fin</th>
+                <th scope="col">Prix</th> 
+                <th scope="col">Descriptif</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php if(!empty($_POST['abos'])) : ?>
+                <?php foreach($abos as $abo): ?>
+                <tr>
+                    <td><?= $abo['nom_client'] ?></td>
+                    <td><?= $abo['prenom'] ?></td>
+                    <td><?= $abo['id_membre'] ?></td>
+                    <td><?= $abo['nom_abo'] ?></td>
+                    <td><?= $abo['duree_abo'] ?></td>
+                    <td><?= $abo['date_inscription'] ?></td>
+                    <td><?= $abo['date_abo'] ?></td>
+                    <td><?= $abo['prix'] ?>€</td>
+                    <td><?= $abo['resum'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+                <?php endif; ?>
+            </tbody>
+        </table>
+
+        <h3 class="text-danger m-1">Ajouter un abonnement</h3>
+        <form method="POST" action="my_cinema2.php">
+            <input type="text" name="id_membre" placeholder="N° client" class="form-control m-2">
+
+            <input type="submit" value="Ajouter" class="btn btn-danger btn-sm m-2">
+        </form>
+
+        <h1 class="text-success m-2">Historique client</h1>
+        <form method="POST" action="my_cinema2.php" class="form-inline">
+            <input type="text" name="histo" placeholder="N° client" class="form-control m-2">
+            <input type="submit" value="Rechercher" class="btn btn-success m-2">
+        </form>
+        <table class="table table-success">
+            <thead>
+            <tr>
+                <th scope="col">N°client</th>
+                <th scope="col">Film</th>
+                <th scope="col">Vu le</th>
+                <th scope="col">Synopsis</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php if(!empty($_POST['histo'])) : ?>
+                <?php foreach($histo as $film): ?>
+                <tr>
+                    <td><?= $film['id_membre'] ?></td>
+                    <td><?= $film['titre'] ?></td>
+                    <td><?= $film['date'] ?></td>
+                    <td><?= $film['resum']?></td>
+                </tr>
+                <?php endforeach; ?>
+                <?php endif; ?>
+            </tbody>
+        </table>
+        <h3 class="text-success m-1">Ajouter un film à l'historique</h3>
+
+        <form method="POST" action="my_cinema2.php">
+            <input type="text" name="id_membre" placeholder="N° client" class="form-control m-2">
+
+            <select name="id_film" id="films"class="custom-select m-2">
+                <option value="">Sélectionnez un film</option>
+                <?php foreach($films as $film): ?>
+                <option value="<?= $film['id_film']; ?>"> <?= $film['titre']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="date">Date</label>
+            <input type="date" id="date" name="date" value="2018-12-22" min="2018-01-01" max="2019-12-31">
+
+            <input type="submit" value="Ajouter" class="btn btn-success btn-sm m-2">
+        </form>
+    </div>
 </body>
 </html>
--- test.php
+++ test.php
@@ -1,21 +1,64 @@
 <?php
 
     $bdd = new PDO('mysql:host=localhost;dbname=epitech_tp;host=127.0.0.1;charset=utf8', 'root', 'root');
-    $sql = 'SELECT *, distrib.nom AS nom_distrib, genre.nom AS nom_genre FROM film INNER JOIN genre ON genre.id_genre=film.id_genre LEFT JOIN distrib ON distrib.id_distrib=film.id_distrib WHERE 1';
-    
+    $movie_query = 'SELECT *, distrib.nom AS nom_distrib, genre.nom AS nom_genre 
+                    FROM film 
+                    INNER JOIN genre ON genre.id_genre=film.id_genre 
+                    LEFT JOIN distrib ON distrib.id_distrib=film.id_distrib 
+                    WHERE 1';
+
     if(isset($_POST["nom"]) && !empty($_POST["nom"])){
-        $sql .= " AND film.titre LIKE '%{$_POST["nom"]}%'";
+        $movie_query .= " AND film.titre LIKE '%{$_POST["nom"]}%'";
     }
     if(isset($_POST["genre"]) && !empty($_POST["genre"])){
-        $sql .= " AND genre.id_genre = {$_POST["genre"]}";
+        $movie_query .= " AND genre.id_genre = {$_POST["genre"]}";
     }
     if(isset($_POST["distribution"]) && !empty($_POST["distribution"])){
-        $sql .= " AND distrib.id_distrib = {$_POST["distribution"]}";
+        $movie_query .= " AND distrib.id_distrib = {$_POST["distribution"]}";
     }
-    $stmt = $bdd->prepare($sql);
+    
+    $stmt = $bdd->prepare($movie_query);
     $stmt->execute();
-    $films = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    //$sql2 = 'SELECT * FROM film INNER JOIN distrib ON distrib.id_distrib=film.id_distrib WHERE 1 ';
-    //$tmp = $bdd->prepare($sql2);
-    //$tmp->execute();
-    //$distrib_table = $tmp->fetchAll(PDO::FETCH_ASSOC);
+    $movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if(isset($_POST["client"]) && !empty($_POST["client"])){
+    $customer_query = "SELECT * from fiche_personne 
+                        INNER JOIN membre ON fiche_personne.id_perso=membre.id_fiche_perso 
+                        WHERE nom LIKE '%{$_POST["client"]}%' OR prenom LIKE '%{$_POST["client"]}%' 
+                        ORDER BY nom ASC";
+    $stmt = $bdd->prepare($customer_query);
+    $stmt->execute();
+    $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+   
+    if(isset($_POST["abos"]) && !empty($_POST["abos"])){
+    $abo_query = "SELECT *, fiche_personne.nom AS nom_client, abonnement.nom AS nom_abo from fiche_personne 
+                    INNER JOIN membre ON fiche_personne.id_perso=membre.id_fiche_perso 
+                    LEFT JOIN abonnement ON abonnement.id_abo=membre.id_abo 
+                    WHERE fiche_personne.nom LIKE '%{$_POST["abos"]}%' OR prenom LIKE '%{$_POST["abos"]}%' 
+                    ORDER BY fiche_personne.nom ASC";
+    $stmt = $bdd->prepare($abo_query);
+    $stmt->execute();
+    $abos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    if(isset($_POST['histo']) && !empty($_POST['histo'])){
+    $histo_query = "SELECT * from historique_membre 
+                    INNER JOIN film ON historique_membre.id_film=film.id_film 
+                    LEFT JOIN membre ON historique_membre.id_membre=membre.id_membre 
+                    WHERE membre.id_membre = {$_POST['histo']} 
+                    ORDER BY date DESC";
+    $stmt = $bdd->prepare($histo_query);
+    $stmt->execute();
+    $histo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    if(isset($_POST['id_membre']) && isset($_POST['id_film']) && isset($_POST['date']) && !empty($_POST['id_membre']) && !empty($_POST['id_film']) && !empty($_POST['date'])){
+    $add_movie_query = "INSERT INTO historique_membre(id_membre, id_film, date) VALUES(:id_membre, :id_film, :date)";
+    $stmt = $bdd->prepare($add_movie_query);
+    $stmt->execute(array(
+        'id_membre' => $_POST['id_membre'],
+        'id_film' =>  $_POST['id_film'],
+        'date' => $_POST['date']
+        ));
+    }
